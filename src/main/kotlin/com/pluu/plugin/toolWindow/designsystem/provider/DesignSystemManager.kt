9c0e5b6e78365aafaf87c9ea6cc07561b35aab6a
pluu <pluulove@gmail.com>
2023-12-24 01:43:24 +0900

    지원하는 파일 갱신

--- src/main/kotlin/com/pluu/plugin/toolWindow/designsystem/provider/DesignSystemManager.kt
+++ src/main/kotlin/com/pluu/plugin/toolWindow/designsystem/provider/DesignSystemManager.kt
@@ -5,8 +5,15 @@ import com.intellij.openapi.project.BaseProjectDirectories.Companion.getBaseDire
 import com.pluu.plugin.toolWindow.designsystem.DesignSystemType
 import com.pluu.plugin.toolWindow.designsystem.model.DesignSystemItem
 import org.jetbrains.android.facet.AndroidFacet
+import javax.imageio.ImageIO
 
 object DesignSystemManager {
+
+    // https://cs.android.com/android-studio/platform/tools/adt/idea/+/mirror-goog-studio-main:android/src/com/android/tools/idea/ui/resourcemanager/plugin/RasterAssetRenderer.kt
+    private val supportImageExtension by lazy {
+        ImageIO.getReaderFormatNames()
+    }
+
     @WorkerThread
     fun findDesignKit(forFacet: AndroidFacet, type: DesignSystemType): List<DesignSystemItem> {
         val project = forFacet.module.project.getBaseDirectories()
@@ -16,6 +23,7 @@ object DesignSystemManager {
             ?.takeIf { it.isDirectory }
             ?: return emptyList()
         return project.children.asSequence()
+            .filter { supportImageExtension.contains(it.extension) }
             .map {
                 DesignSystemItem(
                     type = type,
