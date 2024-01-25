263529d2fc11fd4c6fef4619818271440ea70019
pluu <pluulove@gmail.com>
2024-01-26 01:55:28 +0900

    Configuration 사용 준비

--- src/main/kotlin/com/pluu/plugin/settings/ConfigService.kt
+++ src/main/kotlin/com/pluu/plugin/settings/ConfigService.kt
@@ -1,10 +1,14 @@
 package com.pluu.plugin.settings
 
 import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
 import com.intellij.util.xmlb.XmlSerializerUtil
 
+@Service(Service.Level.PROJECT)
 @State(
     name = "PluuPlugin",
     storages = [Storage("PluuPlugin.xml")]
@@ -16,5 +20,9 @@ class ConfigService : PersistentStateComponent<Config> {
 
     override fun loadState(state: Config) {
         XmlSerializerUtil.copyBean(state, this.config)
+    }
+
+    companion object {
+        fun getInstance(project: Project): ConfigService = project.service()
     }
 }
