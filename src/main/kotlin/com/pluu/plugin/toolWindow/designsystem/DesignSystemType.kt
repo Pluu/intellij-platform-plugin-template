5f071a3e39d70cdaebf1c1c894f69acb8b05d1e4
pluu <pluulove@gmail.com>
2024-02-03 15:45:05 +0900

    미사용 코드 제거

--- src/main/kotlin/com/pluu/plugin/toolWindow/designsystem/DesignSystemType.kt
+++ src/main/kotlin/com/pluu/plugin/toolWindow/designsystem/DesignSystemType.kt
@@ -1,7 +1,5 @@
 package com.pluu.plugin.toolWindow.designsystem
 
-import com.pluu.plugin.settings.ConfigSettings
-
 data class DesignSystemType(
     val name: String,
     val isSelectable: Boolean = true
@@ -11,11 +9,6 @@ data class DesignSystemType(
     companion object {
 
         val NONE = DesignSystemType("", false)
-
-        fun instanceFromConfigure(typeName: String): DesignSystemType {
-            return ConfigSettings.getInstance().getTypes()
-                .first { it.name.equals(typeName, ignoreCase = true) }
-        }
     }
 }
 
