8ecbad82bff95924b60d53b5e540451c2c96dc8a
pluu <pluulove@gmail.com>
2024-04-13 15:47:55 +0900

    XML 입력시 AUTO_URI namespace 추가

--- src/main/kotlin/com/pluu/plugin/toolWindow/designsystem/model/ResourcePasteProvider.kt
+++ src/main/kotlin/com/pluu/plugin/toolWindow/designsystem/model/ResourcePasteProvider.kt
@@ -4,6 +4,8 @@ package com.pluu.plugin.toolWindow.designsystem.model
 // Origin : https://cs.android.com/android-studio/platform/tools/adt/idea/+/mirror-goog-studio-main:android/src/com/android/tools/idea/ui/resourcemanager/model/ResourcePasteProvider.kt
 ///////////////////////////////////////////////////////////////////////////
 
+import com.android.SdkConstants
+import com.android.tools.idea.res.ensureNamespaceImported
 import com.intellij.ide.PasteProvider
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.actionSystem.ActionUpdateThread
@@ -15,7 +17,9 @@ import com.intellij.openapi.editor.actions.PasteAction
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiFile
 import com.intellij.psi.codeStyle.CodeStyleManager
+import com.intellij.psi.xml.XmlFile
 import org.jetbrains.kotlin.idea.KotlinFileType
 
 class ResourcePasteProvider : PasteProvider {
@@ -25,34 +29,37 @@ class ResourcePasteProvider : PasteProvider {
         when (psiFile.fileType) {
             XmlFileType.INSTANCE,
             KotlinFileType.INSTANCE -> {
-                performForCode(dataContext, psiFile.fileType, caret)
+                performForCode(dataContext, psiFile, caret)
             }
         }
     }
 
     private fun performForCode(
         dataContext: DataContext,
-        fileType: FileType,
+        psiFile: PsiFile,
         caret: Caret
     ) {
         val project = CommonDataKeys.PROJECT.getData(dataContext) ?: return
         val item = getDesignSystemItem(dataContext) ?: return
-        if (!isPastSupport(fileType, item.applicableFileType)) return
+        if (!isPastSupport(psiFile.fileType, item.applicableFileType)) return
         val sampleCode = item.sampleCode?.takeIf { it.isNotEmpty() } ?: return
-        pasteAtCaret(caret, sampleCode, project)
+        pasteAtCaret(caret, sampleCode, project, psiFile)
     }
 
-    private fun pasteAtCaret(caret: Caret, text: String, project: Project) {
+    private fun pasteAtCaret(caret: Caret, text: String, project: Project, psiFile: PsiFile) {
         runWriteAction {
             caret.editor.document.insertString(caret.offset, text)
             caret.selectStringFromOffset(text, caret.offset)
 
             // Reformat code
-            val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(caret.editor.document) ?: return@runWriteAction
             val psiDocumentManager = PsiDocumentManager.getInstance(project)
             psiDocumentManager.commitDocument(caret.editor.document)
             CodeStyleManager.getInstance(project).reformatRange(psiFile, caret.selectionStart, caret.selectionEnd)
             psiDocumentManager.doPostponedOperationsAndUnblockDocument(caret.editor.document)
+
+            if (psiFile is XmlFile) {
+                ensureNamespaceImported(psiFile, SdkConstants.AUTO_URI)
+            }
         }
         caret.removeSelection()
         caret.moveToOffset(caret.offset - text.length)
