b7ceeb134430e71a9e67857b09c57274e0e6aff1
pluu <pluulove@gmail.com>
2025-02-28 21:49:56 +0900

    Sample action

---
+++ src/main/kotlin/com/pluu/plugin/action/NonWizardAction.kt
@@ -0,0 +1,63 @@
+package com.pluu.plugin.action
+
+import com.android.tools.idea.device.explorer.DeviceExplorerToolWindowFactory
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.dsl.builder.bindSelected
+import com.intellij.ui.dsl.builder.panel
+import icons.PluuIcons
+import javax.swing.JComponent
+
+class NonWizardAction : AnAction(
+    "[NonWizard] Action",
+    "[NonWizard] Action",
+    PluuIcons.Konata
+), DumbAware {
+    override fun actionPerformed(p0: AnActionEvent) {
+        SampleDialog(p0.project).show()
+    }
+}
+
+class SampleDialog(
+    private val project: Project?
+) : DialogWrapper(project) {
+
+    private var openRunConfigWhenDone = true
+
+    init {
+        init()
+    }
+
+    override fun createCenterPanel(): JComponent = panel {
+        row {
+            checkBox("Option label")
+                .label("Check")
+                .bindSelected(::openRunConfigWhenDone)
+        }
+    }
+
+    override fun doOKAction() {
+        super.doOKAction()
+        notify("[OK] Result : $openRunConfigWhenDone")
+    }
+
+    override fun doCancelAction() {
+        super.doCancelAction()
+        notify("[Cancel click]")
+    }
+
+    private fun notify(message: String) {
+        val notification = Notification(
+            DeviceExplorerToolWindowFactory.TOOL_WINDOW_ID,
+            "Project opened detected",
+            message,
+            NotificationType.INFORMATION
+        )
+        notification.notify(project)
+    }
+}
---
+++ src/main/kotlin/com/pluu/plugin/action/WizardStepAction.kt
@@ -0,0 +1,74 @@
+package com.pluu.plugin.action
+
+import com.android.tools.idea.device.explorer.DeviceExplorerToolWindowFactory
+import com.android.tools.idea.wizard.model.ModelWizard
+import com.android.tools.idea.wizard.model.ModelWizardStep
+import com.android.tools.idea.wizard.ui.StudioWizardDialogBuilder
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.ui.dsl.builder.actionListener
+import com.intellij.ui.dsl.builder.bindSelected
+import com.intellij.ui.dsl.builder.panel
+import icons.PluuIcons
+import javax.swing.JComponent
+
+class WizardStepAction : AnAction(
+    "[Wizard Step] Action",
+    "[Wizard Step] Action",
+    PluuIcons.Konata
+), DumbAware {
+    override fun actionPerformed(p0: AnActionEvent) {
+        val wizard = ModelWizard.Builder()
+            .addStep(AehdInstallInfoStep(p0.project))
+            .build()
+        StudioWizardDialogBuilder(wizard, ">>>")
+            .setProject(p0.project)
+            .build()
+            .show()
+    }
+}
+
+class AehdInstallInfoStep(
+    private val project: Project?
+) : ModelWizardStep.WithoutModel("Emulator Settings") {
+
+    private var isChecked = true
+
+    private val panel = panel {
+        row {
+            label(
+                "<html>This wizard will execute Android Emulator hypervisor driver stand-alone installer."
+                        + " This is an additional step required to install this package.</html>"
+            )
+        }
+        row {
+            checkBox("Option label")
+                .label("Check")
+                .bindSelected(::isChecked)
+                .actionListener {_, it ->
+                    isChecked = it.isSelected
+                }
+        }
+    }
+
+    override fun getComponent(): JComponent = panel
+
+    override fun onWizardFinished() {
+        super.onWizardFinished()
+        notify("[OK] Result : ${isChecked}")
+    }
+
+    private fun notify(message: String) {
+        val notification = Notification(
+            DeviceExplorerToolWindowFactory.TOOL_WINDOW_ID,
+            "Project opened detected",
+            message,
+            NotificationType.INFORMATION
+        )
+        notification.notify(project)
+    }
+}
