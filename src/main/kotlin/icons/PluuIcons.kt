f93938e3b92262a932f932845d8486a190d62fb3
pluu <pluulove@gmail.com>
2025-05-22 00:28:11 +0900

    Add icon

--- src/main/kotlin/icons/PluuIcons.kt
+++ src/main/kotlin/icons/PluuIcons.kt
@@ -5,17 +5,11 @@ import javax.swing.Icon
 
 // Icon format : https://plugins.jetbrains.com/docs/intellij/icons.html#icon-formats
 object PluuIcons {
-    private fun load(path: String): Icon {
+    fun load(path: String): Icon {
         return IconLoader.getIcon(path, javaClass)
     }
 
     @JvmField
     val Konata: Icon = load("/icons/konata.png")
-
-    val iconButton = load("/icons/icon_button.svg")
-    val iconSlider = load("/icons/icon_slider.svg")
-    val iconText = load("/icons/icon_text.svg")
-    val iconToast = load("/icons/icon_toast.svg")
-    val iconNone = load("/icons/icon_none.svg")
 }
 
