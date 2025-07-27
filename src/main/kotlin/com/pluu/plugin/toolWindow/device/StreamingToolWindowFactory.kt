5fd67eee42812138ceefd926de3f7b9373f97a0e
pluu <pluulove@gmail.com>
2025-07-27 19:22:46 +0900

    Update rename

--- src/main/kotlin/com/pluu/plugin/toolWindow/device/StreamingToolWindowFactory.kt
+++ src/main/kotlin/com/pluu/plugin/toolWindow/device/StreamingToolWindowFactory.kt
@@ -9,7 +9,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 
-class DeviceManagerToolWindowFactory : ToolWindowFactory, DumbAware {
+class StreamingToolWindowFactory : ToolWindowFactory, DumbAware {
     override fun init(toolWindow: ToolWindow) {
         DeviceToolWindowManager(toolWindow)
     }
