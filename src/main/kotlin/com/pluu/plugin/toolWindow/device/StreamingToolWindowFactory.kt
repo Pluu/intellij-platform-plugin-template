d8356512ee36739d3ed9451c35d8aff32409b871
pluu <pluulove@gmail.com>
2025-07-27 19:59:27 +0900

    Update ToolWindowFactory

--- src/main/kotlin/com/pluu/plugin/toolWindow/device/StreamingToolWindowFactory.kt
+++ src/main/kotlin/com/pluu/plugin/toolWindow/device/StreamingToolWindowFactory.kt
@@ -4,16 +4,39 @@
 
 package com.pluu.plugin.toolWindow.device
 
+import com.intellij.icons.AllIcons
+import com.intellij.ide.actions.ToolWindowWindowAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowContentUiType
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.openapi.wm.ToolWindowType
+import com.intellij.openapi.wm.ex.ToolWindowEx
+import com.intellij.openapi.wm.impl.content.ToolWindowContentUi
 
 class StreamingToolWindowFactory : ToolWindowFactory, DumbAware {
-    override fun init(toolWindow: ToolWindow) {
-        DeviceToolWindowManager(toolWindow)
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        toolWindow.setTitleActions(listOf(MoveToWindowAction(toolWindow)))
+        toolWindow.setDefaultContentUiType(ToolWindowContentUiType.TABBED)
     }
 
-    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+    override fun init(toolWindow: ToolWindow) {
+        toolWindow.component.putClientProperty(ToolWindowContentUi.ALLOW_DND_FOR_TABS, true)
+        toolWindow.component.putClientProperty(ToolWindowContentUi.DONT_HIDE_TOOLBAR_IN_HEADER, true)
+        DeviceToolWindowManager(toolWindow as ToolWindowEx)
+    }
+
+    private class MoveToWindowAction(private val toolWindow: ToolWindow) : ToolWindowWindowAction() {
+        override fun update(event: AnActionEvent) {
+            when (toolWindow.type) {
+                ToolWindowType.FLOATING, ToolWindowType.WINDOWED -> event.presentation.isEnabledAndVisible = false
+                else -> {
+                    super.update(event)
+                    event.presentation.icon = AllIcons.Actions.MoveToWindow
+                }
+            }
+        }
     }
 }
